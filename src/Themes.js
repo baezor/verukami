258f93f346a17608b8ae6dabd8af3b866029fbb1
Angel Baez <angel@uptrending.com>
2019-05-02 23:08:17 -0500

    :tada: Created Themes.js

---
+++ src/Themes.js
@@ -0,0 +1,17 @@
+const Dark = {
+  colors: {
+    primary: 'black',
+    background: '#E9ECF0'
+  },
+  fixedFrame: 'linear-gradient(135deg, rgba(189,124,240,1) 0%, rgba(5,169,223,1) 100%)'
+}
+const Light = {
+  colors: {
+    primary: '#FBF2EC',
+    background: '#1C2426'
+  },
+  fixedFrame: '#FBF2EC'
+}
+
+export const dark = Dark;
+export const light = Light;
