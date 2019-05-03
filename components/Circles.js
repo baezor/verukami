f516cf38b88c0cd32e00616ca31e949a8d0699b3
Angel Baez <angel@uptrending.com>
2019-05-02 23:02:56 -0500

    Created Circles component.

---
+++ components/Circles.js
@@ -0,0 +1,11 @@
+const Circles = () => (
+  <div>
+    <div className="circle is-1" />
+    <div className="circle is-2" />
+    <div className="circle is-3" />
+    <div className="circle is-4" />
+    <div className="circle is-5" />
+    <div className="circle is-6" />
+  </div>
+)
+export default Circles
