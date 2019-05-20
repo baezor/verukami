b433895e9252dff61de202f7376837938b7bfb07
Angel Baez <angel@uptrending.com>
2019-05-20 09:56:08 -0500

    🛠️Updated WorkPreview component

--- components/WorkPreview.js
+++ components/WorkPreview.js
@@ -1,24 +1,57 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import demo from './assets/demo.png';
+import ButtonCircle from './ButtonCircle';
+
+
+
+
+const Container = styled.section`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Preview  = styled.div`
+  width: 100%;
+  max-width: 70%;
+  margin: 0 auto;
+  height: auto;
+  display: flex;
+`
+const Image = styled.div(
+  {
+    flex: '0 1 50%'
+  },
+  props => ({ order: props.order })
+)
+
+const Content = styled.div(
+  {
+    flex: '1 0 50%',
+    padding: '100px 40px'
+  },
+  props => ({ order: props.order })
+)
 
 class WorkPreview extends React.Component {
   render(){
+    const { layout } = this.props;
     return (
-      <section class="block block--work">
-      <div class="block-wrapper">
-        <div class="block-content">
-            <div class="block-float-title">WORK</div>
-            <div class="block-image block-image--left">
-              <img src="img/demo.png" alt="Nebvla: HR Dashboard design" />
-            </div>
-            <div class="block-meta block-meta--right">
-              <span class="meta-title">Product Design</span>
-              <h2>Nebvla: <br/>HR dashboard design</h2>
-              <a class="btn btn-circle" href=""><div class="btn-circle__inner"><span>READ <span class="bold">MORE</span></span></div></a>
-            </div>
-          </div>
-      </div>
-    </section>
-
+      <Container>
+        <Preview>
+          <Image order={ (layout == 'textFirst') ? 1 : 2 }> 
+            <img className="img-responsive" src={demo} alt={'Work Preview'}  />
+          </Image>
+          <Content order={ (layout == 'textFirst') ? 2 : 1 }>
+            <span className="meta-title">Product Design</span>
+            <h2>Nebvla: <br/>HR dashboard design</h2>
+            <ButtonCircle />
+          </Content>
+        </Preview>
+      </Container>
     )
   }
 }
